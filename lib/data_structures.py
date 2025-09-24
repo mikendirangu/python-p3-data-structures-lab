6393027cf83f5828a42c390dcec1f5c9b21fe369
Big mike <mikendirangu26@gmail.com>
2025-09-24 20:40:13 +0300

    Completed Data Structures Lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -36,3 +36,29 @@ def get_average_heat_level(spicy_foods):
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
+
+def get_names(foods):
+    return [food["name"] for food in foods]
+
+def get_spiciest_foods(foods):
+    return [food for food in foods if food["heat_level"] > 5]
+
+def print_spicy_foods(foods):
+    for food in foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶'*food['heat_level']}")
+
+def get_spicy_food_by_cuisine(foods, cuisine):
+    for food in foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+def print_spiciest_foods(foods):
+    for food in get_spiciest_foods(foods):
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶'*food['heat_level']}")
+
+def get_average_heat_level(foods):
+    return sum(f["heat_level"] for f in foods) // len(foods)
+
+def create_spicy_food(foods, new_food):
+    foods.append(new_food)
+    return foods
